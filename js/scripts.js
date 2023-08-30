e7f626ee400532787aabd89bb6d0a607bdbb2c33
Dustin Hofstetter <hofstetter.dustinart@gmail.com>
2023-08-29 22:00:22 -0400

    Added a navbar file and need to add bar graphic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 /*on load this should show*/
+fetch('/nBar.html')
+    .then(response => response.text())
+    .then(content => {
+        document.getElementById('nBar').innerHTML = content;
+    });
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " nn1-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top nn1-white", "");
+    }
+}
+
 fetch('/profiles/objective.html')
     .then(response => response.text())
     .then(content => {
